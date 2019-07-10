53650a53a41d77d6838c7ec931b793b11cded339
TIGER为梦想而生 <1763548953@qq.com>
2019-07-10 14:33:18 +0800

    爬楼梯

---
+++ pkg/ClimbStairs.java
@@ -0,0 +1,17 @@
+package pkg;
+/**
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * 
+ * */
+public class ClimbStairs {
+	public int climbStairs(int n) {
+        return n;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClimbStairs test = new ClimbStairs();
+		System.out.println(test.climbStairs(3));
+	}
+
+}
