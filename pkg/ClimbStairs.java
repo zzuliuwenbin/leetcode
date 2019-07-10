8f571206c044cf3c448585524659449e2144fed0
TIGER为梦想而生 <1763548953@qq.com>
2019-07-10 17:41:05 +0800

    实现爬楼梯算法

--- pkg/ClimbStairs.java
+++ pkg/ClimbStairs.java
@@ -4,14 +4,35 @@ package pkg;
  * 
  * */
 public class ClimbStairs {
+	//排列组合公式计算，如果可以优化到o(1)时间内计算出来组合数，算法应该还算挺快
+	public int C(int a,int b) {
+		long t1 = 1, t2 = 1;
+		for(int i=b,j=1;i>b-a;i--) {
+			t1 = t1*i;t2 = t2*j;j++;
+			if(t1%t2==0) {//计算到结尾再求结果会溢出，用t1如果用int，在第43个测试用例时依旧溢出了
+				t1 = t1/t2;
+				t2 = 1;
+			}
+		}
+		return (int)(t1/t2);
+	}
 	public int climbStairs(int n) {
-        return n;
+		int sum = 0;
+		for(int i=0;i<=n/2;i++) {
+			if(i>(n-i)/2) {//利用组合公式的特点减少计算量
+				sum = sum + C(n-2*i,n-i);
+			}else {
+				sum = sum + C(i,n-i);
+			}
+			
+		}
+        return sum;
     }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ClimbStairs test = new ClimbStairs();
-		System.out.println(test.climbStairs(3));
+		System.out.println(test.climbStairs(44));
 	}
 
 }
