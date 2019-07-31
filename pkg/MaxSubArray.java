9cc4849dac397ff183372719185351f3326050a4
刘文斌 <liuwenbin@liuwenbin.local>
2019-07-31 10:56:04 +0800

    最大子序和

--- pkg/MaxSubArray.java
+++ pkg/MaxSubArray.java
@@ -3,9 +3,21 @@ package pkg;
 public class MaxSubArray {
 
 	public int maxSubArray(int[] nums) {
-		//test
-		//test2
-		return 0; 
+		int sum[] = new int[nums.length];
+		sum[0] = nums[0];
+		for(int i=1;i<nums.length;i++) {
+			if(sum[i-1]>0){//sum[i-1]+nums[i]>nums[i]
+				sum[i] = sum[i-1]+nums[i];
+			}else {
+				sum[i] = nums[i];
+			}
+		}
+		for(int j=1;j<sum.length;j++) {
+			if(sum[j]>sum[0]) {
+				sum[0] = sum[j];
+			}
+		}
+		return sum[0]; 
     }
 
 	public static void main(String[] args) {
