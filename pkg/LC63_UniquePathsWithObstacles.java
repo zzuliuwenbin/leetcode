7d22c9920883724182c5eec07cb760e034e66866
刘文斌 <zzuliuwenbin@126.com>
2019-11-25 20:37:37 +0800

    机器人行走路径总和

--- pkg/LC63_UniquePathsWithObstacles.java
+++ pkg/LC63_UniquePathsWithObstacles.java
@@ -15,11 +15,14 @@ public class LC63_UniquePathsWithObstacles {
 		int n = obstacleGrid[0].length;
 		int map[][] = new int[m][n];
 		
+		//遇到障碍物之前都是1，遇到障碍物之后，后面的相当于不可达，不再初始化（默认初始化的就是0）
 		int i=1;
 		while(i<n&&obstacleGrid[0][i]==0) map[0][i++] = 1;
 		int j=0;
 		while(j<m&&obstacleGrid[j][0]==0) map[j++][0] = 1;
 		
+		//从上到下，从左到右，没有障碍物的地方就直接等于左前和上方的值的和
+		//有障碍物的话就把该点的值设置为和为0，因为后面的计算会用到这点，而这点是不可达的
 		for(i=1;i<m;i++) {
 			for(j=1;j<n;j++) {
 				if(obstacleGrid[i][j]==1) {
